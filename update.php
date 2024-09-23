d59ca5992e1b6f7cec4aa1c4aeee0dd75a621d5c
Ivan Podoprigorov <ivan.podoprigorov@mail.ru>
2024-09-23 13:54:42 +0500

    hashpass

--- update.php
+++ update.php
@@ -1,18 +1,31 @@
 <?php
-    $UserID=$_POST['id_user'];
-    require_once "conectdb.php";
-    $queryUserCheck = mysqli_query($conn, "SELECT * FROM `users` WHERE `id_user`=$UserID"); 
-    $user = mysqli_fetch_array($queryUserCheck);
+session_start();
+require_once "conectdb.php";
 
-    $FIO = $_POST["FIO"];
-    $login = $_POST["login"];
-    $email = $_POST["email"];
-    $password = $_POST["password"]; 
-    
-    $sql="UPDATE `users` SET `FIO`='$FIO',`login`='$login',`email`='$email',`password`='$password' WHERE `id_user`=$UserID";
-    if($conn->query($sql)){
-        header('Location:account.php');
-    } else{
-        echo "Ошибка: " . $conn->error;
-    }
+$UserID = $_SESSION['id'];
+
+$queryUserCheck = mysqli_query($conn, "SELECT * FROM users WHERE id_user='$UserID'"); 
+$user = mysqli_fetch_array($queryUserCheck);
+
+$FIO = $_POST["FIO"];
+$login = $_POST["login"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "UPDATE users SET email='$email', FIO='$FIO', login='$login', password='$hashedPassword' WHERE id_user='$UserID'";
+
+if ($conn->query($sql)) {
+    header('Location: account.php');
+    exit;
+} else {
+    echo "Ошибка: " . $conn->error;
+}
+if (!empty($password)) {
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "UPDATE users SET email='$email', FIO='$FIO', login='$login', password='$hashedPassword' WHERE id_user='$UserID'";
+} else {
+    $sql = "UPDATE users SET email='$email', FIO='$FIO', login='$login' WHERE id_user='$UserID'";
+}
 ?>
